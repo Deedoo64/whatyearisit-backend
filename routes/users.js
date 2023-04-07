cd7c9e0f8feba34647715bf38f04f99fecd10969
Didier LASCAUX <dlascaux@yahoo.fr>
2023-04-07 10:35:15 +0200

    Move route /year in users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/year", (req, res) => {
+  const date = new Date();
+  res.json({ year: date.getFullYear() });
 });
 
 module.exports = router;
